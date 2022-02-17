050e79e4a5c34bb79d81b2ecf66ea4883d39b846
savchenko_v <savchenko_v@mail.n.o>
2022-02-17 18:14:27 +0300

    послед-сть до максимального указанного значения

--- main.js
+++ main.js
@@ -13,3 +13,37 @@ function fibonacci(number) {
     }
     return result;
 }
+
+function maxValue(maxNumber) {  // вывод последовательности до максимального указанного значения
+    var a = 0;
+    var b = 1;
+    result = [];
+    result.push(a, b);
+    while (b < maxNumber) {
+        var c = a + b;
+        a = b;
+        b = c;
+        if (b <= maxNumber) {
+            result.push(b);
+        }
+    }
+    return result;
+}
+
+function maxValue2(maxNumber) {  // второй способ решения
+    var a = 0;
+    var b = 1;
+    result = [a, b];
+    c = a + b;
+    while (c <= maxNumber) {
+        a = b;
+        b = c;
+        result.push(c);
+        c = a + b;
+    }
+    return result;
+}
+
+console.log(fibonacci(24));
+console.log(maxValue(8));
+console.log(maxValue2(34));
